84260813bafdf0fe2032563f20c50c2cd12ec184
ren_hai_peng <1730172964@qq.com>
2020-08-19 17:46:54 +0800

    添加公共模块,并测试通过

---
+++ kmall-api/src/main/java/com/kgc/kmall/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.kgc.kmall.service;
+
+import com.kgc.kmall.bean.Member;
+
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2020-08-19 17:37
+ */
+public interface MemberService {
+
+    public List<Member> selectAllMember();
+}
